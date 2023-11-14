d7fd89264548bceea48d54d5113a07d4a5c13dda
Eva GRABOCZ <egrabocz@leonard-solutions.com>
2023-11-14 17:34:54 +0100

    Formulaire contact

--- TD4/docs/js/exercices.js
+++ TD4/docs/js/exercices.js
@@ -68,36 +68,69 @@
 
 
 
-const select = document.querySelector('#select');
-select.addEventListener('change', function() {
-    const Ville = select.options[select.selectedIndex].value;
-    // const selectedOption = select.options[select.selectedIndex];
-    const url_img = ""
-    if (Ville == "Budapest") {
-        document.querySelector('#image').src = "../../assets/images/budapest.jpg"
-        url_img = "../../assets/images/budapest.jpg"
-    }
-    else if (Ville == "Caen") {
-        document.querySelector('#image').src = "../../assets/images/caen.jpg"
-        url_img = "../../assets/images/caen.jpg"
-    }
-    else if (Ville == "Paris") {
-        document.querySelector('#image').src = "../../assets/images/paris.jpg"
-        url_img = "../../assets/images/paris.jpg"
-    }
-    else if (Ville == "Espagne") {
-        document.querySelector('#image').src = "../../assets/images/espagne.jpg" 
-        url_img = "../../assets/images/espagne.jpg" 
-    }
-    else if (Ville == "Vermont") {
-        document.querySelector('#image').src = "../../assets/images/vermont.jpg"
-        url_img = "../../assets/images/vermont.jpg"
-    }
-    else if (Ville == "Montreal") {
-        document.querySelector('#image').src = "../../assets/images/montreal.jpg"
-        url_img = "../../assets/images/montreal.jpg"
-    }
-    document.querySelector('#image').src = url_img
-});
+// const select = document.querySelector('#select');
+// select.addEventListener('change', function() {
+//     const Ville = select.options[select.selectedIndex].value;
+//     // const selectedOption = select.options[select.selectedIndex];
+//     const url_img = ""
+//     if (Ville == "Budapest") {
+//         document.querySelector('#image').src = "../../assets/images/budapest.jpg"
+//         url_img = "../../assets/images/budapest.jpg"
+//     }
+//     else if (Ville == "Caen") {
+//         document.querySelector('#image').src = "../../assets/images/caen.jpg"
+//         url_img = "../../assets/images/caen.jpg"
+//     }
+//     else if (Ville == "Paris") {
+//         document.querySelector('#image').src = "../../assets/images/paris.jpg"
+//         url_img = "../../assets/images/paris.jpg"
+//     }
+//     else if (Ville == "Espagne") {
+//         document.querySelector('#image').src = "../../assets/images/espagne.jpg" 
+//         url_img = "../../assets/images/espagne.jpg" 
+//     }
+//     else if (Ville == "Vermont") {
+//         document.querySelector('#image').src = "../../assets/images/vermont.jpg"
+//         url_img = "../../assets/images/vermont.jpg"
+//     }
+//     else if (Ville == "Montreal") {
+//         document.querySelector('#image').src = "../../assets/images/montreal.jpg"
+//         url_img = "../../assets/images/montreal.jpg"
+//     }
+//     document.querySelector('#image').src = url_img
+// });
 
 // méthode switch case à voir
+
+
+// Over element et alerte 
+
+// const p = document.querySelector('#card');
+// hoverAlert(p);
+
+// function hoverAlert(p) {
+//     p.addEventListener('mouseover', function() {
+//       alert('Le survol fonctionne' + ' !');
+//     });
+//   }
+
+
+
+// log des infos dans un formulaire de contact 
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const nom = document.querySelector("#nom").value;
+  const email = document.querySelector("#email").value;
+  const message = document.querySelector("#message").value;
+
+  console.log("Nom :", nom);
+  console.log("Adresse e-mail :", email);
+  console.log("Message :", message);
+});
+
+
+
